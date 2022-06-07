877d3fa5f8d27480bdd4589005304a807b26d823
Stefan Fink <65467915+finkstefan@users.noreply.github.com>
2022-06-07 23:41:55 +0200

    Upisivanje porudzbina i stavki porudzbina u bazu

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import {TableContainer,Table,TableHead,TableBody,TableRow,TableCell,Paper,Button} from "@mui/material"
 
+
 function Products(){
 
     const [products, setProducts] = useState([]);
     const [orderItems,setOrderItems]= useState([]);
+    const [itemsCounted,setItemsCounted]= useState([]);
+
+    var randomString = require("random-string");
 
     
     useEffect(() => {
@@ -23,23 +27,82 @@ function Products(){
        
     };
 
+    function addOrder(){
+      var orderId = randomString({
+        length: 8,
+        numeric: true,
+        letters: false,
+        special: false,
+        
+        });
+
+        const keyedArr = orderItems.reduce((accumulator, currentValue) => {
+          const key = currentValue.toString();
+          if (!(key in accumulator))
+            accumulator[key] = 1;
+          else
+            accumulator[key] += 1;
+            
+          return accumulator;
+        }, {});
+        console.log(keyedArr)
+        setItemsCounted(keyedArr);
+
+        postOrder(orderId);
+        
+
+  Object.entries(keyedArr).map(([key, value]) => (
+        
+        postOrderItem(key,value,orderId)
+      )); 
+      
+    }
+
     function addOrderItem(id) {
+     
         setOrderItems(prevState => [...prevState, id]);
         console.log(orderItems)
       }
 
-      function confirmOrder(){
-        const keyedArr = orderItems.reduce((accumulator, currentValue) => {
-            const key = currentValue.toString();
-            if (!(key in accumulator))
-              accumulator[key] = 1;
-            else
-              accumulator[key] += 1;
-              
-            return accumulator;
-          }, {});
-          console.log(keyedArr)
+      function postOrderItem(id,count,orderId) {
+        var orderItemId = randomString({
+          length: 8,
+          numeric: true,
+          letters: false,
+          special: false,
+          
+          });
+        const item = { stavkaPorudzbineId: orderItemId,proizvodId:id,porudzbinaId:orderId,kolicina:count };
+
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(item)
+      };
+
+console.log(JSON.stringify(item));
+
+      fetch('http://localhost:4250/api/stavkaPorudzbine', requestOptions)
+          .then(response => console.log(response))
       }
+
+       function postOrder(id) {
+        var todayDate = new Date().toISOString().slice(0, 10);
+        const order = { porudzbinaId: id,datum:todayDate,distributerId:4,Isplacena:false };
+
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(order)
+        }
+
+        console.log(JSON.stringify(order))
+        
+      fetch('http://localhost:4250/api/porudzbina', requestOptions)
+      .then(response => console.log(response))
+      };
+
+      
 
     return (<div>
         <h2>Proizvodi</h2>
@@ -66,7 +129,7 @@ function Products(){
        </Table>
         </TableContainer>
         <br/>
-        <Button variant="contained" onClick={() => {confirmOrder()}}>Potvrdi porudzbinu</Button>
+        <Button variant="contained" onClick={() => {addOrder()}}>Potvrdi porudzbinu</Button>
             
             
         </div>)
