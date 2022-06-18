429f0a622ed8ec970c1f91c82a67257764c0b248
Stefan Fink <65467915+finkstefan@users.noreply.github.com>
2022-06-18 23:23:16 +0200

    Uzimanje vrednosti iz dijaloga i priprema za post proizvoda

--- src/components/ProductDialog.jsx
+++ src/components/ProductDialog.jsx
@@ -6,9 +6,21 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import { useEffect, useState, useRef } from "react";
 
 export default function ProductDialog() {
   const [open, setOpen] = React.useState(false);
+  const [productName, setProductName] = React.useState('');
+  const [productPrice, setProductPrice] = React.useState(0);
+  const [productAvailable, setProductAvailable] = React.useState(true);
+
+  var randomString = require("random-string");
+
+ // const name = useRef();
+  
+  //const price = useRef();
+  
+ // const available = useRef();
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -18,33 +30,99 @@ export default function ProductDialog() {
     setOpen(false);
   };
 
+  const handleConfirm = () => {
+    setProductName(name)
+   setProductPrice(price)
+   setProductAvailable(available)
+
+    console.log(productName);
+  };
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState(0);
+  const [available, setAvailable] = useState(false);
+
+  const handleNameChange = event => {
+    setName(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
+  const handlePriceChange = event => {
+    setPrice(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
+  const handleAvailableChange = event => {
+    setAvailable(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+
+  function postProduct() {
+    var productId = randomString({
+      length: 8,
+      numeric: true,
+      letters: false,
+      special: false,
+      
+      });
+  //  const item = { proizovdId: productId,kategorijaId:,kolicina:count };
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+     // body: JSON.stringify(item)
+  };
+
   return (
     <div>
       <Button variant="outlined" onClick={handleClickOpen}>
         Novi proizvod
       </Button>
       <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
+        <DialogTitle>Novi proizvod:</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
           <TextField
             autoFocus
             margin="dense"
             id="name"
-            label="Email Address"
-            type="email"
+       
+            label="Naziv proizvoda"
+            type="text"
+            onChange={handleNameChange}
             fullWidth
             variant="standard"
           />
+           <TextField
+            autoFocus
+            margin="dense"
+            id="price"
+            onChange={handlePriceChange}
+            label="Cena"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+            <TextField
+            autoFocus
+            margin="dense"
+            id="available"
+            onChange={handleAvailableChange}
+            label="Dostupan"
+            type="checkbox"
+            fullWidth
+            variant="standard"
+          />
+           
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
+          <Button onClick={handleClose}>Zatvori</Button>
+          <Button onClick={handleConfirm}>Dodaj</Button>
         </DialogActions>
       </Dialog>
     </div>
   );
+  }
 }
