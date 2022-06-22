e64ad0ea9a43c5149dc6419f50bf335570d399e9
Stefan Fink <65467915+finkstefan@users.noreply.github.com>
2022-06-22 21:49:20 +0200

    Dodato rutiranje i dizajn login stranice

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Pages from "./pages/Pages"
 function App() {
   return (
     <div className="App">
-     <h1>Trebovanje</h1>
+     <h1>ProCure - online trebovanje</h1>
      <Pages />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -12,3 +12,46 @@ root.render(
 );
 
 
+import React from 'react';  
+import ReactDOM from 'react-dom';  
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'  
+import './index.css';  
+import App from './App';  
+import Login from './components/Login'  
+
+  
+const routing = (  
+  <Router>  
+    <div>  
+    
+     <Route path="/" component={App} />  
+      <Route path="/login" component={Login} />  
+    
+    
+      
+    </div>  
+  </Router>  
+)  
+ReactDOM.render(routing, document.getElementById('root'));  
+*/
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import App from "./App";
+import Login from "./components/Login";
+
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<Login />} />
+ 
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
